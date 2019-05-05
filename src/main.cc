a22a38605db7c8fe5c6f3809fc6521f75f5fb38a
Your Name <you@example.com>
2019-05-05 15:36:49 +0800

    add WOCA

--- src/main.cc
+++ src/main.cc
@@ -50,41 +50,53 @@ float cent[32768];
 float WPCA_w[134217728];
 float WPCA_b[4096];
 
-void ListImages(string const &path, vector<string> &images) {
-    images.clear();
-    struct dirent *entry;
+float f_result[294912];
+float after_softmax[36864];
+float vlad[32768];
+float fin[4096];
 
-    /*Check if path is a valid directory path. */
-    struct stat s;
-    lstat(path.c_str(), &s);
-    if (!S_ISDIR(s.st_mode)) {
-        fprintf(stderr, "Error: %s is not a valid directory!\n", path.c_str());
-	exit(1);
-    }
+void ListImages(string const &path, vector<string> &images)
+{
+	images.clear();
+	struct dirent *entry;
 
-    DIR *dir = opendir(path.c_str());
-    if (dir == nullptr) {
-        fprintf(stderr, "Error: Open %s path failed.\n", path.c_str());
-	exit(1);
-    }
+	/*Check if path is a valid directory path. */
+	struct stat s;
+	lstat(path.c_str(), &s);
+	if (!S_ISDIR(s.st_mode))
+	{
+		fprintf(stderr, "Error: %s is not a valid directory!\n", path.c_str());
+		exit(1);
+	}
 
-    while ((entry = readdir(dir)) != nullptr) {
-        if (entry->d_type == DT_REG || entry->d_type == DT_UNKNOWN) {
+	DIR *dir = opendir(path.c_str());
+	if (dir == nullptr)
+	{
+		fprintf(stderr, "Error: Open %s path failed.\n", path.c_str());
+		exit(1);
+	}
+
+	while ((entry = readdir(dir)) != nullptr)
+	{
+		if (entry->d_type == DT_REG || entry->d_type == DT_UNKNOWN)
+		{
 			string name = entry->d_name;
 			string ext = name.substr(name.find_last_of(".") + 1);
-			if ((ext == "JPEG") || (ext == "jpeg") || (ext == "JPG") || (ext == "jpg") || (ext == "PNG") || (ext == "png")) {
+			if ((ext == "JPEG") || (ext == "jpeg") || (ext == "JPG") || (ext == "jpg") || (ext == "PNG") || (ext == "png"))
+			{
 				images.push_back(name);
 			}
 		}
-    }
+	}
 
-    closedir(dir);
+	closedir(dir);
 }
 
-void out_file(DPUTask* task) {
+void out_file(DPUTask *task)
+{
 	int num = dpuGetOutputTensorSize(task, OUTPUT_NODE);
-	int8_t* result = new int8_t[num];
-	cout<<num<<endl;
+	int8_t *result = new int8_t[num];
+	cout << num << endl;
 	/*dpuGetOutputTensorInHWCInt8(task, OUTPUT_NODE, result, num);
 	//result = dpuGetOutputTensorAddress(task, OUTPUT_NODE);
 	ofstream outfile("result_HWC.txt", ios::out);
@@ -100,15 +112,18 @@ void out_file(DPUTask* task) {
 	dpuGetOutputTensorInCHWInt8(task, OUTPUT_NODE, result, num);
 	//result = dpuGetOutputTensorAddress(task, OUTPUT_NODE);
 	ofstream outfile1("result_CHW.txt", ios::out);
-	if(!outfile1) {
-		cerr<<"open outfile erro"<<endl;
+	if (!outfile1)
+	{
+		cerr << "open outfile erro" << endl;
 		exit(1);
 	}
-	for(int i=0; i<num; i++) {
-		result[i]=(+result[i]);
+	for (int i = 0; i < num; i++)
+	{
+		result[i] = (+result[i]);
 	}
-	for(int i=0; i<num; i++) {
-		outfile1<<result[i]*2<<" ";
+	for (int i = 0; i < num; i++)
+	{
+		outfile1 << result[i] * 2 << " ";
 	}
 	outfile1.close();
 	delete[] result;
@@ -143,115 +158,152 @@ void out_file(DPUTask* task) {
 	delete[] result;
 }*/
 
-void normalize(float *data) {
-	for (int i = 0; i < 576; i++) {
+void normalize(float *data)
+{
+	for (int i = 0; i < 576; i++)
+	{
 		float sum = 0;
-		for (int j = 0; j < 512; j++) {
+		for (int j = 0; j < 512; j++)
+		{
 			sum = sum + data[j * 576 + i] * data[j * 576 + i];
 		}
 		sum = sqrt(sum);
-		if (sum < 1e-12) sum = 1e-12;
-		for (int j = 0; j < 512; j++) {
+		if (sum < 1e-12)
+			sum = 1e-12;
+		for (int j = 0; j < 512; j++)
+		{
 			data[j * 576 + i] = data[j * 576 + i] / sum;
 		}
 	}
 }
 
-void conv(float *data, float *a, float *w) {
-	for (int i = 0; i < 36864; i++) {
+void conv(float *data, float *a, float *w)
+{
+	for (int i = 0; i < 36864; i++)
+	{
 		a[i] = 0;
 	}
-	for (int i = 0; i < 64; i++) {
-		for (int j = 0; j < 576; j++) {
-			for (int k = 0; k < 512; k++) {
+	for (int i = 0; i < 64; i++)
+	{
+		for (int j = 0; j < 576; j++)
+		{
+			for (int k = 0; k < 512; k++)
+			{
 				a[i * 576 + j] = a[i * 576 + j] + data[k * 576 + j] * w[512 * i + k];
 			}
 		}
 	}
-	for (int i = 0; i < 576; i++) {
+	for (int i = 0; i < 576; i++)
+	{
 		float sum = 0;
-		for (int j = 0; j < 64; j++) {
+		for (int j = 0; j < 64; j++)
+		{
 			a[j * 576 + i] = a[j * 576 + i] - 360;
 			sum = sum + exp(a[j * 576 + i]);
 		}
-		for (int j = 0; j < 64; j++) {
+		for (int j = 0; j < 64; j++)
+		{
 			a[j * 576 + i] = exp(a[j * 576 + i]) / sum;
 		}
 	}
 }
 
-void vlad_core(float *data, float *a, float *v, float *c) {
-	float* temp = new float[64 * 512 * 576];
-	for (int i = 0; i < 64; i++) {
-		for (int j = 0; j < 512; j++) {
-			for (int k = 0; k < 576; k++) {
+void vlad_core(float *data, float *a, float *v, float *c)
+{
+	float *temp = new float[64 * 512 * 576];
+	for (int i = 0; i < 64; i++)
+	{
+		for (int j = 0; j < 512; j++)
+		{
+			for (int k = 0; k < 576; k++)
+			{
 				temp[i * 576 * 512 + j * 576 + k] = data[j * 576 + k] + c[i * 512 + j];
 				temp[i * 576 * 512 + j * 576 + k] *= a[576 * i + k];
 			}
 		}
 	}
-	for (int i = 0; i < 64; i++) {
-		for (int j = 0; j < 512; j++) {
-			for (int k = 0; k < 576; k++) {
+	for (int i = 0; i < 64; i++)
+	{
+		for (int j = 0; j < 512; j++)
+		{
+			for (int k = 0; k < 576; k++)
+			{
 				v[i * 512 + j] += temp[i * 512 * 576 + j * 576 + k];
 			}
 		}
 	}
 }
 
-void normalize2(float *vlad) {
-	float* temp = new float[32768];
-	for (int i = 0; i < 64; i++) {
+void normalize2(float *vlad)
+{
+	float *temp = new float[32768];
+	for (int i = 0; i < 64; i++)
+	{
 		float sum = 0;
-		for (int j = 0; j < 512; j++) {
+		for (int j = 0; j < 512; j++)
+		{
 			sum = sum + vlad[j + i * 512] * vlad[j + i * 512];
 		}
 		sum = sqrt(sum);
-		if (sum < 1e-12) sum = 1e-12;
-		for (int j = 0; j < 512; j++) {
+		if (sum < 1e-12)
+			sum = 1e-12;
+		for (int j = 0; j < 512; j++)
+		{
 			temp[j * 64 + i] = vlad[j + i * 512] / sum;
 		}
 	}
 	float sum = 0;
-	for (int i = 0; i < 32768; i++)sum += (temp[i] * temp[i]);
+	for (int i = 0; i < 32768; i++)
+		sum += (temp[i] * temp[i]);
 	sum = sqrt(sum);
-	for (int i = 0; i < 32768; i++)vlad[i] = (temp[i] / sum);
+	for (int i = 0; i < 32768; i++)
+		vlad[i] = (temp[i] / sum);
 }
 
-void fc(float* v, float* f, float* w, float* b) {
-	for (int i = 0; i < 4096; i++)f[i] = 0;
-	for (int i = 0; i < 4096; i++) {
-		for (int j = 0; j < 32768; j++) {
+void fc(float *v, float *f, float *w, float *b)
+{
+	for (int i = 0; i < 4096; i++)
+		f[i] = 0;
+	for (int i = 0; i < 4096; i++)
+	{
+		for (int j = 0; j < 32768; j++)
+		{
 			f[i] = f[i] + v[j] * w[i * 32768 + j];
 		}
 		f[i] = f[i] + b[i];
 	}
 }
 
-void normalize3(float* f) {
+void normalize3(float *f)
+{
 	float sum = 0;
-	for (int i = 0; i < 4096; i++) {
+	for (int i = 0; i < 4096; i++)
+	{
 		sum += f[i] * f[i];
 	}
 	sum = sqrt(sum);
-	for (int i = 0; i < 4096; i++) {
+	for (int i = 0; i < 4096; i++)
+	{
 		f[i] = f[i] / sum;
 	}
 }
 
-void run_netvlad(DPUTask *task, float* conv_w, float* cent, float* WPCA_w, float* WPCA_b) {
+void run_netvlad(DPUTask *task, float *conv_w, float *cent, float *WPCA_w, float *WPCA_b)
+{
 	assert(task);
 	vector<string> images;
 	ListImages(baseImagePath, images);
-	if (images.size() == 0) {
-        	cerr << "\nError: Not images exist in " << baseImagePath << endl;
-        	return;
-       	}
+	if (images.size() == 0)
+	{
+		cerr << "\nError: Not images exist in " << baseImagePath << endl;
+		return;
+	}
 
-	for(auto &imageName : images) {
-    	cout<< "\nLoad image : " << imageName << endl;
+	for (auto &imageName : images)
+	{
+		cout << "\nLoad image : " << imageName << endl;
 		Mat image = imread(baseImagePath + imageName);
-		float mean[3]={0,0,0};
+		float mean[3] = {0, 0, 0};
 		//cvtColor(image, image, );
 		dpuSetInputImage(task, INPUT_NODE, image, mean);
 
@@ -262,67 +314,63 @@ void run_netvlad(DPUTask *task, float* conv_w, float* cent, float* WPCA_w, float
 		/* Get DPU execution time (in us) of CONV Task */
 		long long timeProf = dpuGetTaskProfile(task);
 		cout << "  DPU CONV Execution time: " << (timeProf * 1.0f) << "us\n";
-		
+
 		int num = dpuGetOutputTensorSize(task, OUTPUT_NODE);
-	    int8_t* result = new int8_t[num];
-		float* f_result = new float[num];
+		int8_t *result = new int8_t[num];
 		dpuGetOutputTensorInCHWInt8(task, OUTPUT_NODE, result, num);
-		for(int i=0; i<num; i++) {
-		    f_result[i]=(+result[i])*2;
-	    }
+		for (int i = 0; i < num; i++)
+		{
+			f_result[i] = (+result[i]) * 2;
+		}
 		delete[] result;
 
 		normalize(f_result);
-		float* after_softmax = new float[36864];
 		conv(f_result, after_softmax, conv_w);
-		//cout << after_softmax[0] << " " << after_softmax[1] << " " << after_softmax[32767];
-		float* vlad = new float[32768];
-		for (int i = 0; i < 32768; i++)vlad[i] = 0;
+		for (int i = 0; i < 32768; i++)
+			vlad[i] = 0;
 		vlad_core(f_result, after_softmax, vlad, cent);
-		delete[] f_result;
-		delete[] after_softmax;
 
 		normalize2(vlad);
 
-		cout<< vlad[0] << " " << vlad[1] << " " << vlad[32767];
+		fc(vlad, fin, WPCA_w, WPCA_b);
+		normalize3(fin);
+
+		//cout<< fin[0] << " " << fin[1] << " " << fin[4095]<<endl;
 
 		//out_file(task);
-    }
-	
-	
-    
+	}
 }
 
-int main(void) {
+int main(void)
+{
 
-    // Attach to DPU driver and prepare for running
-    dpuOpen();
+	// Attach to DPU driver and prepare for running
+	dpuOpen();
 
-    // Load DPU Kernel for DenseBox neural network
-    DPUKernel *kernel = dpuLoadKernel("netvlad");
+	// Load DPU Kernel for DenseBox neural network
+	DPUKernel *kernel = dpuLoadKernel("netvlad");
 
 	DPUTask *task;
 	task = dpuCreateTask(kernel, 0);
 
 	//load vlad parameters
 	fstream in("/home/linaro/netvlad/model/vlad_weight", ios::in | ios::binary);
-	in.read((char*)conv_weight, 32768*sizeof(float));
-    in.read((char*)cent, 32768*sizeof(float));
-    in.read((char*)WPCA_w, 134217728*sizeof(float));
-    in.read((char*)WPCA_b, 4096*sizeof(float));
-    in.close();
+	in.read((char *)conv_weight, 32768 * sizeof(float));
+	in.read((char *)cent, 32768 * sizeof(float));
+	in.read((char *)WPCA_w, 134217728 * sizeof(float));
+	in.read((char *)WPCA_b, 4096 * sizeof(float));
+	in.close();
 
-	
-    // Doing face detection.
-    run_netvlad(task, conv_weight, cent, WPCA_w, WPCA_b);
+	// Doing face detection.
+	run_netvlad(task, conv_weight, cent, WPCA_w, WPCA_b);
 
 	dpuDestroyTask(task);
 
-    // Destroy DPU Kernel & free resources
-    dpuDestroyKernel(kernel);
+	// Destroy DPU Kernel & free resources
+	dpuDestroyKernel(kernel);
 
-    // Dettach from DPU driver & release resources
-    dpuClose();
+	// Dettach from DPU driver & release resources
+	dpuClose();
 
-    return 0;
+	return 0;
 }
